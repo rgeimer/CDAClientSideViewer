3b76010c39d1cb41bc435c9530041ed21226333b
rgeimer <rick.geimer@lantanagroup.com>
2014-05-22 13:15:06 -0400

    Moved to SaxonCE. Got it working in IE11.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 var myApp = angular.module('myApp',[]);
 
+var support  = {
+    // Are files exposed to JS?
+    // As used by Modernizr @
+    // https://github.com/Modernizr/Modernizr/blob/master/feature-detects/file/api.js
+    'fileReader'  : (function testFileReader(){
+        // Test: look for global file class.
+        return !!(window.File && window.FileList && window.FileReader);
+    }()),
+
+    // AJAX file upload via formData?
+    'formData'    : window.FormData !== void 0
+};
+
 function loadXMLDoc(filename){
     xhttp = new XMLHttpRequest();
     xhttp.open("GET", filename, false);
@@ -9,7 +22,11 @@ function loadXMLDoc(filename){
 
 function RenderController($scope) {
     var xsl = loadXMLDoc("cda.xsl");
-    $scope.fileData = 'none';
+    if (support.fileReader == true){
+        $scope.fileData = 'Please select a file and click the Display CDA button.';
+    } else {
+        $scope.fileData = 'Sorry, your browser is not supported.';
+    }
     $scope.displayCDA = function(){
         var f = document.getElementById('file').files[0];
         var r = new FileReader();
@@ -18,14 +35,14 @@ function RenderController($scope) {
             var domParser = new DOMParser();
             var xml = r.result;
             var xmlDom = domParser.parseFromString(xml, 'text/xml');
-            var xsltProcessor = new XSLTProcessor();
+            var xsltProcessor = Saxon.newXSLT20Processor();
             xsltProcessor.importStylesheet(xsl);
             var resultDocument = xsltProcessor.transformToFragment(xmlDom, document);
-            $scope.fileData.innerHTML = resultDocument;
+            $scope.fileData = resultDocument.html;
             document.getElementById("example").appendChild(resultDocument);
         }
 
-        r.readAsBinaryString(f);
+        r.readAsText(f);
     }
 }
 
